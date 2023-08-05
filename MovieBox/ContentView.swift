6d67c623146dfaddd26a6e7fb4b0ff2e9f972625
Özenç Yeni <ozencyeni@gmail.com>
2023-08-05 21:00:46 +0300

    Initial Commit

---
+++ MovieBox/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieBox
+//
+//  Created by Özenç Yeni on 5.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieBox/MovieBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBoxApp.swift
+//  MovieBox
+//
+//  Created by Özenç Yeni on 5.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
